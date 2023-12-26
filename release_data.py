19eb23fef10ee6ab32c727aab41ba175b53b54f2
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-26 12:22:58 +0530

    Create release_data.py

---
+++ release_data.py
@@ -0,0 +1,16 @@
+{
+  "busObId": "string",
+  "cacheKey": "string",
+  "cacheScope": "Tenant",
+  "fields": [
+    {
+      "dirty": true,
+      "displayName": "string",
+      "fieldId": "string",
+      "html": "string",
+      "name": "string",
+      "value": "string"
+    }
+  ],
+  "persist": true
+}
