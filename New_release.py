ade874d864a1cca313d9ed362bc09e3171a855c4
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-26 11:01:38 +0530

    Update New_release.py

--- New_release.py
+++ New_release.py
@@ -2,7 +2,25 @@ import requests
 import base64
 
 def check_cherwell_connection(api_url, username, password):
-    # ... (existing connection check code)
+    # Endpoint for a basic Cherwell API call (you might want to adjust this based on Cherwell's API)
+    endpoint = f"{api_url}/api/V1/heartbeat"
+
+    # Set up headers with content type and authorization
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": get_authorization_header(username, password)
+    }
+
+    # Make a simple request to check the connection
+    response = requests.get(endpoint, headers=headers)
+
+    # Check if the connection was successful (status code 200)
+    if response.status_code == 200:
+        print("Connection to Cherwell successful!")
+        return True
+    else:
+        print(f"Failed to connect to Cherwell. Status code: {response.status_code}")
+        return False
 
 def create_new_release(api_url, username, password):
     if not check_cherwell_connection(api_url, username, password):
@@ -58,9 +76,15 @@ def create_new_release(api_url, username, password):
     else:
         print(f"Failed to create new release. Status code: {create_response.status_code}")
 
+def get_authorization_header(username, password):
+    # Helper function to generate the Authorization header
+    credentials = f"{username}:{password}"
+    encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
+    return f"Basic {encoded_credentials}"
+
 # Example usage
 api_url = ""  # Adjust the URL
 username = ""
 password = ""
 
-create_new_release(api_url, username, password)￼Enter
+create_new_release(api_url, username, password)
